36155b659874c11327127cb1ebad583bcfd71703
PiNull <null@llun.com>
2016-09-04 00:48:38 +0900

    face

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const
   request = require('request');
 const util = require('util');
 
-var faceModule = require('./lib/facepp-sdk.js').Face;
+var faceModule = require('./lib/facepp-sdk.js');
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
@@ -260,7 +260,7 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
-    switch (messageText) {
+    switch (messageText.toLowerCase()) {
       case 'pick':
         sendPickMessage(senderID);
         break;
@@ -436,7 +436,7 @@ function receivedAccountLink(event) {
                 //obj.style.backgroundColor = colorName;
                 //container.innerHTML += "<img src='"+obj.value+"' width='100px'>";
             
-                var api = new faceModule.FacePP('0ef14fa726ce34d820c5a44e57fef470', '4Y9YXOMSDvqu1Ompn9NSpNwWQFHs1hYD');
+                var api = new faceModule.Face.FacePP('0ef14fa726ce34d820c5a44e57fef470', '4Y9YXOMSDvqu1Ompn9NSpNwWQFHs1hYD');
                 api.request('detection/detect', {
                   url: obj.value //'http://cn.faceplusplus.com/static/resources/python_demo/1.jpg'
                 }, function(err, result) {
@@ -478,7 +478,7 @@ function sendPickMessage(recipientId) {
                 //obj.style.backgroundColor = colorName;
                 //container.innerHTML += "<img src='"+obj.value+"' width='100px'>";
             
-                var api = new faceModule.FacePP('0ef14fa726ce34d820c5a44e57fef470', '4Y9YXOMSDvqu1Ompn9NSpNwWQFHs1hYD');
+                var api = new faceModule.Face.FacePP('0ef14fa726ce34d820c5a44e57fef470', '4Y9YXOMSDvqu1Ompn9NSpNwWQFHs1hYD');
                 api.request('detection/detect', {
                   url: obj.value //'http://cn.faceplusplus.com/static/resources/python_demo/1.jpg'
                 }, function(err, result) {
