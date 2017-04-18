bd63f50dd329dc3f0aaf04f27f7b2d747a97394c
PiNull <null@llun.com>
2017-04-19 04:20:17 +0900

    latest

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ const Vision = require('@google-cloud/vision');
 // Read the file into memory.
 var fs = require('fs');
 //var imageFile = fs.readFileSync('https://scontent.xx.fbcdn.net/v/t34.0-12/18009221_1472598482812362_1866440958_n.jpg?_nc_ad=z-m&oh=b60b2345dfb525c57f3f48864458ea77&oe=58F6BDED');
+/*
 const file = fs.createWriteStream("image.jpg");
 
 https.get("https://scontent.xx.fbcdn.net/v/t34.0-12/18009221_1472598482812362_1866440958_n.jpg", response => {
@@ -59,6 +60,7 @@ var imageFile = fs.readFileSync('image.jpg');
 // Covert the image data to a Buffer and base64 encode it.
 var encoded = new Buffer(imageFile).toString('base64');
 console.log(encoded);
+*/
 
 // Your Google Cloud Platform project ID
 const projectId = 'translate-0';
@@ -563,8 +565,19 @@ function receivedMessage(event) {
       case 'vision': //Google Vision
       case 'read':
       case 'analysis':
+      case 'hashtag':
       case '분석':
+      case '👓':
         sendVisionMessage(senderID);
+        break;
+            
+      case 'find': //Google Vision
+      case 'web':
+      case 'detect':
+      case '검색':
+      case '🔍':
+      case '🔎':
+        sendVisionWebMessage(senderID);
         break;
             
             
@@ -650,7 +663,17 @@ function receivedMessage(event) {
     sendTextMessage(senderID, "사진에서 1명의 새로운 얼굴을 인식했습니다.");
       console.log("SBPN1 "+messageAttachments);
       console.log("SBPN2 "+url);
-      console.log("SBPN3 "+util.inspect(messageAttachments, false, null))
+      console.log("SBPN3 "+util.inspect(messageAttachments, false, null));
+      //request(url).pipe(fs.createWriteStream('temp.jpg'));
+    /*request(url)
+      .pipe(pipeTo)
+      .on('finish', function() {
+        assert.ok(fs.statSync(pipeTo.path), 'Did not create destination path');
+        var source = fs.readFileSync(url).toString();
+        var destination = fs.readFileSync(pipeTo.path).toString();
+        assert.equal(source, destination);
+        console.log("SBPN4 "+pipeTo.path);
+      });*/
       
     callFaceAPI('detect', url);
   }
@@ -906,7 +929,7 @@ function sendIUMessage(recipientId) {
     var personId = '2c0681cb-5d2c-4d10-b287-ab7910c26eb7';
     var len = facesMS.length;
     var result = "FRAS needs new photo for comparison"
-    if(len > 0) {
+    if(len > 0 && !(typeof facesMS[len-1].faceId)) {
         msFace.api('verify', 'POST', {}, {
           faceId: facesMS[len-1].faceId,
           personId: personId,
@@ -999,16 +1022,17 @@ function sendVisionMessage(recipientId) {
         console.log("##### SBPN ##### URL "+obj);
         var result = "I can see";
         var hashtag = "FRAS"
-
+/***************************************************************/
         request(obj).pipe(fs.createWriteStream('temp.jpg'));
         // Read the file into memory.
-// Covert the image data to a Buffer and base64 encode it.
+        // Covert the image data to a Buffer and base64 encode it.
         
-            setTimeout(
-            function(){
-                var tmp = fs.readFileSync('temp.jpg');
-                var encoded = new Buffer(tmp).toString('base64');
-                console.log("##### SBPN ##### Base64 "+encoded);
+        setTimeout(
+        function(){
+        
+        //var tmp = fs.readFileSync('temp.jpg');
+        //var encoded = new Buffer(tmp).toString('base64');
+        //console.log("##### SBPN ##### Base64 "+encoded);
                 
 visionClient.detectLabels('temp.jpg')
   .then((results) => {
@@ -1017,28 +1041,217 @@ visionClient.detectLabels('temp.jpg')
     console.log('Labels:');
     labels.forEach((label) => {
         console.log(label);
-        result+=" #"+label.replace(/(\s)/g, "_");
+        result += " #"+label.replace(/(\s*)/g, "_");
     });
     
-    hashtag=labels[0];
-    hashtag=hashtag.replace(/(\s)/g, "_");
+    hashtag = labels[0];
+    hashtag = hashtag.replace(/(\s*)/g, "");
   })
   .catch((err) => {
-    console.error('ERROR on Vision :', err);
-    //console.info(err.errors[0].errors[0]);
+    console.error('ERROR in Vision :', err);
+    console.info(err.errors[0].errors[0]);
     //result = JSON.stringify(err.errors[0].errors[0]);
+    result = err;
     
   });
             
-            },2000);
-      
-
+    },1000);
+/***************************************************************/      
+        
         setTimeout(
             function(){
                 sendTextMessage(recipientId, result);
                 console.log(result);
-                sendTextMessage(recipientId, "More photo on Insta - http://www.imgrum.org/tag/"+hashtag);
-            },3000);
+                sendTextMessage(recipientId, "More photos on Insta - http://www.imgrum.org/tag/"+hashtag);
+            },1000);
+    }
+    else {
+        messageData = {
+            recipient: {
+              id: recipientId
+            },
+            message: {
+              text: "Show me any photo which you want to analyze :O",
+              metadata: "Typing_off"
+            }
+        };
+        callSendAPI(messageData);
+    }
+}
+function sendVisionWebMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    sender_action: "typing_on"
+  };
+  callSendAPI(messageData);
+    
+    var urls = messageAttachedImages;
+    var len = urls.length;
+    if(len > 0) {
+        var obj = urls[len-1];
+        console.log("##### SBPN ##### URL for Vision "+obj);
+/***************************************************************/
+        request(obj).pipe(fs.createWriteStream('temp.jpg'));
+        // Read the file into memory.
+        // Covert the image data to a Buffer and base64 encode it.
+
+    setTimeout(function(){
+        //var tmp = fs.readFileSync('temp.jpg');
+        //var encoded = new Buffer(tmp).toString('base64');
+        //console.log("##### SBPN ##### Base64 "+encoded);
+                
+// Detect similar images on the web to a local file
+visionClient.detectSimilar('temp.jpg')
+  .then((data) => {
+    var results = data[1].responses[0].webDetection;
+
+    if (results.fullMatchingImages.length > 0) {
+      console.log(`Full matches found: ${results.fullMatchingImages.length}`);
+      results.fullMatchingImages.forEach((image) => {
+        console.log(`  URL: ${image.url}`);
+        console.log(`  Score: ${image.score}`);
+        //Showing
+        var messageData = {
+            recipient: { id: recipientId },
+            message: {attachment: {type: "image", payload: {
+                  url: image.url }}}};
+        callSendAPI(messageData);
+        sendTextMessage(recipientId, "Full matches "+image.score+"%");
+      });
+    }
+
+    if (results.partialMatchingImages.length > 0) {
+      console.log(`Partial matches found: ${results.partialMatchingImages.length}`);
+      results.partialMatchingImages.forEach((image) => {
+        console.log(`  URL: ${image.url}`);
+        console.log(`  Score: ${image.score}`);
+        //Showing
+        var messageData = {
+            recipient: { id: recipientId },
+            message: {attachment: {type: "image", payload: {
+                  url: image.url }}}};
+        callSendAPI(messageData);
+        sendTextMessage(recipientId, "Partial matches "+image.score+"%");
+      });
+    }
+    
+    
+    setTimeout(function(){
+    if (results.webEntities.length > 0) {
+      console.log(`Web entities found: ${results.webEntities.length}`);
+        
+    function compare(a, b) {
+        return parseInt(a.score) < parseInt(b.score) ? -1 : parseInt(a.score) > parseInt(b.score) ? 1 : 0;
+    }
+    results.webEntities.sort(compare);
+        
+      results.webEntities.forEach((webEntity) => {
+        console.log(`  Description: ${webEntity.description}`);
+        console.log(`  Score: ${webEntity.score}`);
+        sendTextMessage(recipientId, "["+webEntity.score+"] "+webEntity.description);
+      });
+    }
+    },2000);
+  });
+            },2000);
+/***************************************************************/      
+
+    }
+    else {
+        messageData = {
+            recipient: {
+              id: recipientId
+            },
+            message: {
+              text: "Show me any photo which you want to analyze :O",
+              metadata: "Typing_off"
+            }
+        };
+        callSendAPI(messageData);
+    }
+}
+function sendVisionColorMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    sender_action: "typing_on"
+  };
+  callSendAPI(messageData);
+    
+    var urls = messageAttachedImages;
+    var len = urls.length;
+    if(len > 0) {
+        var obj = urls[len-1];
+        console.log("##### SBPN ##### URL for Vision "+obj);
+/***************************************************************/
+        request(obj).pipe(fs.createWriteStream('temp.jpg'))
+        // Read the file into memory.
+        // Covert the image data to a Buffer and base64 encode it.
+
+    setTimeout(function(){
+        //var tmp = fs.readFileSync('temp.jpg');
+        //var encoded = new Buffer(tmp).toString('base64');
+        //console.log("##### SBPN ##### Base64 "+encoded);
+                
+// Detect similar images on the web to a local file
+visionClient.detectSimilar('temp.jpg')
+  .then((data) => {
+    const results = data[1].responses[0].webDetection;
+
+    if (results.fullMatchingImages.length > 0) {
+      console.log(`Full matches found: ${results.fullMatchingImages.length}`);
+      results.fullMatchingImages.forEach((image) => {
+        console.log(`  URL: ${image.url}`);
+        console.log(`  Score: ${image.score}`);
+        //Showing
+        var messageData = {
+            recipient: { id: recipientId },
+            message: {attachment: {type: "image", payload: {
+                  url: image.url }}}};
+        callSendAPI(messageData);
+        sendTextMessage(recipientId, "Full matches "+image.score+"%");
+      });
+    }
+
+    if (results.partialMatchingImages.length > 0) {
+      console.log(`Partial matches found: ${results.partialMatchingImages.length}`);
+      results.partialMatchingImages.forEach((image) => {
+        console.log(`  URL: ${image.url}`);
+        console.log(`  Score: ${image.score}`);
+        //Showing
+        var messageData = {
+            recipient: { id: recipientId },
+            message: {attachment: {type: "image", payload: {
+                  url: image.url }}}};
+        callSendAPI(messageData);
+        sendTextMessage(recipientId, "Partial matches "+image.score+"%");
+      });
+    }
+    
+    
+    setTimeout(function(){
+    if (results.webEntities.length > 0) {
+      console.log(`Web entities found: ${results.webEntities.length}`);
+        
+    function compare(a, b) {
+    return parseInt(a.score) < parseInt(b.score) ? -1 : parseInt(a.score) > parseInt(b.score) ? 1 : 0;
+    }
+    webEntity.sort(compare);
+        
+      results.webEntities.forEach((webEntity) => {
+        console.log(`  Description: ${webEntity.description}`);
+        console.log(`  Score: ${webEntity.score}`);
+        sendTextMessage(recipientId, "["+webEntity.score+"] "+webEntity.description);
+      });
+    }
+    },2000);
+  });
+            },2000);
+/***************************************************************/      
+
     }
     else {
         messageData = {
@@ -1388,9 +1601,9 @@ function sendClearMessage(recipientId) {
     sendTextMessage(recipientId, "I have forgotten all my memories. T_T Could you resend your photo? :O");
 }
 function sendHelpMessage(recipientId) {
-    sendTextMessage(recipientId, "First of all, send your photos and next,");
-    sendTextMessage(recipientId, "You can say including 'pick', 'clear/reset', 'all/show me', 'clear/reset', 'face/compare' or 'IU'. Plus, 'how old', 'vision/read/analysis', and so on. :D");
-    sendTextMessage(recipientId, "'IU' will compare between your photo and the face of IU.");
+    sendTextMessage(recipientId, "First of all, send me your photos and next,");
+    sendTextMessage(recipientId, "you can say command including 'pick', 'clear/reset', 'all/show me', 'clear/reset', 'face/compare' or 'IU'. Plus, 'how old', 'vision/read/hashtag/analysis/분석', 'find/web/detect/검색', and so on. :D");
+    sendTextMessage(recipientId, "'IU' will compare between your photo and the face of IU who is famous Korean singer.");
 }
 function sendImageMessage(recipientId) {
   var messageData = {
