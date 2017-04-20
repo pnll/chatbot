0e3a911c9bdb259a1c41d25ac93e07c5ca5a279e
PiNull <null@llun.com>
2017-04-20 20:07:49 +0900

    .

--- index.js
+++ index.js
@@ -396,6 +396,36 @@ function receivedAuthentication(event) {
 var messageAttachedImages = new Array();
 var facesMS = new Array();
 
+// Speed up calls to hasOwnProperty
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+function isEmpty(obj) {
+
+    // null and undefined are "empty"
+    if (obj == null) return true;
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    // If it isn't an object at this point
+    // it is empty, but it can't be anything *but* empty
+    // Is it empty?  Depends on your application.
+    if (typeof obj !== "object") return true;
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    if (hasOwnProperty.call(obj, 'url')) return false;
+
+    return true;
+}
+
 function receivedMessage(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
@@ -662,8 +692,10 @@ function receivedMessage(event) {
         //sendTextMessage(senderID, messageText);
     }
   } else if (messageAttachments) {
-      var url = messageAttachments[0].payload.url;
-      if (!url) {
+      
+      var url = "";
+      if (isEmpty(messageAttachments[0].payload)) {
+        url = messageAttachments[0].payload.url;
         messageAttachedImages.push(url);
       }
     //sendTextMessage(senderID, "Message with attachment received");
