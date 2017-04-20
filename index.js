3a55defb9d8edaae790f8eb7ac9d0a5d8488c9e1
PiNull <null@llun.com>
2017-04-20 20:34:17 +0900

    .

--- index.js
+++ index.js
@@ -396,36 +396,6 @@ function receivedAuthentication(event) {
 var messageAttachedImages = new Array();
 var facesMS = new Array();
 
-// Speed up calls to hasOwnProperty
-var hasOwnProperty = Object.prototype.hasOwnProperty;
-
-function isEmpty(obj) {
-
-    // null and undefined are "empty"
-    if (obj == null) return true;
-
-    // Assume if it has a length property with a non-zero value
-    // that that property is correct.
-    if (obj.length > 0)    return false;
-    if (obj.length === 0)  return true;
-
-    // If it isn't an object at this point
-    // it is empty, but it can't be anything *but* empty
-    // Is it empty?  Depends on your application.
-    if (typeof obj !== "object") return true;
-
-    // Otherwise, does it have any properties of its own?
-    // Note that this doesn't handle
-    // toString and valueOf enumeration bugs in IE < 9
-    for (var key in obj) {
-        if (hasOwnProperty.call(obj, key)) return false;
-    }
-
-    if (hasOwnProperty.call(obj, 'url')) return false;
-
-    return true;
-}
-
 function receivedMessage(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
@@ -695,7 +665,7 @@ function receivedMessage(event) {
       
       var url = "";
       console.log(JSON.stringify(messageAttachments[0]));
-      if (hasOwnProperty.call(messageAttachments[0], 'payload')) {
+      if (!messageAttachments[0].payload) {
         url = messageAttachments[0].payload.url;
         messageAttachedImages.push(url);
       }
